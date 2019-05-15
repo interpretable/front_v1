a0a40b80e39b60067a4e12c6b69b969905274b32
ysiouda <ysiouda@erasme.org>
2019-05-15 11:48:43 +0200

    style Fixes

--- public/js/app.js
+++ public/js/app.js
@@ -213,8 +213,8 @@ $(document).ready(function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /var/www/interpretable/interpretabble-front/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /var/www/interpretable/interpretabble-front/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/interpretable/front/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/interpretable/front/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
